d79a7da53c9270999c71027db32b171ed109bfb0
Bean <tang_bean@163.com>
2023-04-23 21:35:18 +0800

    test url route: Round 3

--- wxcloudrun/apps/message/__init__.py
+++ wxcloudrun/apps/message/__init__.py
@@ -2,13 +2,9 @@ from django.apps import AppConfig
 
 
 class MessageAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'wxcloudrun.apps.message'
     label = 'message'
     verbose_name = 'Message'
-
-    def ready(self):
-        pass
 
 
 default_app_config = 'wxcloudrun.apps.message.MessageAppConfig'
