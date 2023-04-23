c79ce76c1810935dc8dd550b0014a03c13fb1adc
Bean <tang_bean@163.com>
2023-04-23 21:47:08 +0800

    test url route: Round 4

--- wxcloudrun/apps/message/urls.py
+++ wxcloudrun/apps/message/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from .views import MessageDispatcherAPIView
 
 
+app_name = 'message'
 urlpatterns = [
     url(r'^message/?$', MessageDispatcherAPIView.as_view()),
 ]
