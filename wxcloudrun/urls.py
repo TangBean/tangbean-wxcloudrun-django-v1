4732af9a1f3c4ab22ffc8e0e8b2820c54dea2a44
Bean <tang_bean@163.com>
2023-04-23 22:04:14 +0800

    test url route: Round 7

--- wxcloudrun/urls.py
+++ wxcloudrun/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.conf.urls import include, url
 from wxcloudrun.apps.message import urls as message_urls
 
+
 urlpatterns = (
     url(r'^api/', include((message_urls, 'message'), namespace='message')),
 )
